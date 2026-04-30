bad4cfe5988c48cbc46ab17a4b2c6ce320fe7003
zockerbude111-web <zockerbude111@gmail.com>
2026-04-30 22:00:14 +0200

    t

---
+++ node_modules/.vite/deps/react-dom.js
@@ -0,0 +1,5 @@
+import {
+  require_react_dom
+} from "./chunk-T5JGD4MA.js";
+import "./chunk-KIIQ52NO.js";
+export default require_react_dom();
---
+++ node_modules/.vite/deps/react.js
@@ -0,0 +1,4 @@
+import {
+  require_react
+} from "./chunk-KIIQ52NO.js";
+export default require_react();
